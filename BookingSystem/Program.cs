bd99d6776f97a4e140365ff2d5e84d1c03245ff0
Danylo <danil.dovhopolyi@gmail.com>
2026-05-13 14:54:26 +0300

    Fixed comments

--- BookingSystem/Program.cs
+++ BookingSystem/Program.cs
@@ -1,11 +1,26 @@
-﻿class Apartment
+﻿public class Apartment
 {
     public int Id { get; set; }
+    public string Address { get; set; }
+    public int Rooms { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"[ID: {Id}] {Address} | Rooms: {Rooms} | Price: {Price}";
+    } 
 }
-class Host
+public class Host
 {
-    public int  Id { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
     public List<Apartment> Apartments { get; set; } = new();
+
+    public override string ToString()
+    {
+        return $"[ID: {Id}] {Name} | Phone: {Phone} | Apartments: {Apartments.Count}:";
+    }
 }
 
 class Program
@@ -14,18 +29,57 @@ class Program
     {
         var hosts = new List<Host>
         {
-            new Host
+            new()
             {
-                Id = 1, Apartments = new List<Apartment> { new Apartment { Id = 1 }, new Apartment { Id = 2 } }
+                Id = 1,
+                Name = "Саша",
+                Phone = "09999999999",
+                Apartments = new List<Apartment>
+                {
+                    new()
+                    { 
+                        Id = 1, 
+                        Address = "Вулиця Пушкина 1",
+                        Rooms = 10,
+                        Price = 2
+                    }, 
+                    new() 
+                    { 
+                        Id = 2, 
+                        Address = "Вулиця Пушкина 2",
+                        Rooms = 10,
+                        Price = 2
+                    }, 
+                    new()
+                    {
+                        Id = 3,
+                        Address = "Вулиця Пушкина 3",
+                        Rooms = 10,
+                        Price = 2
+                    }, 
+                    new()
+                    {
+                        Id = 4,
+                        Address = "Вулиця Пушкина 4",
+                        Rooms = 10,
+                        Price = 2
+                    }
+                }
             },
-            new Host
+            new()
             {
-                Id = 2, Apartments = new List<Apartment> { new Apartment { Id = 3 } }
+                Id = 2, 
+                Name = "Даня",
+                Phone = "09999999991",
+                Apartments = new List<Apartment> { new() { Id = 5 } }
             },
-            new Host
+            new()
             {
-                Id = 3, Apartments = new List<Apartment>
-                    { new Apartment { Id = 4 }, new Apartment { Id = 5 }, new Apartment { Id = 6 } }
+                Id = 3, 
+                Name = "Тест",
+                Phone = "09999999992",
+                Apartments = new List<Apartment>
+                    { new() { Id = 6 }, new() { Id = 7 }, new() { Id = 8 } }
             },
         };
 
@@ -33,24 +87,23 @@ class Program
         
         Console.WriteLine("Choose host:");
         Console.WriteLine(string.Join(", ", hosts.Select(h => h.Id)));
-
-        var chosenHostId = int.Parse(Console.ReadLine());
+        if (!int.TryParse(Console.ReadLine(), out var chosenHostId))
+        {
+            Console.WriteLine("Could not  parse choice"); 
+            return;
+        }
         var chosenHost = hosts.FirstOrDefault(h => h.Id == chosenHostId);
 
-        if (chosenHost == null)
+        if (chosenHost is null)
         {
             Console.WriteLine("Host not found");
             return;
         }
-
-        Console.WriteLine($"Apartments of {chosenHost.Id}:");
-        foreach (var a in chosenHost.Apartments)
+        
+        Console.WriteLine(chosenHost);
+        foreach (var apartment in chosenHost.Apartments)
         {
-            Console.WriteLine($"Apartment ID: {a.Id}");
+            Console.WriteLine(apartment);
         }
-        
-        
     }
-
-  
 }
